69b7485d16ef26181635661039150dd501e67a5e
Sess254 <wamaritemoses@gmail.com>
2023-08-13 20:11:48 +0300

    bugs

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,15 @@
 
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class_dict = {
-        'BaseModel': BaseMode,
+        'BaseModel': BaseModel,
         'User': User,
         'State': state,
         'City': City,
